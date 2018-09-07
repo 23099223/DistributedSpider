fc7f7c606d2ea4b2ba102f6b30a3144e1d9c131c
23099223 <23099223@qq.com>
2018-09-07 23:27:53 +0800

    修改返回信息的key 增加失败url列表

--- com/hxl/notebooks/notes.py
+++ com/hxl/notebooks/notes.py
@@ -6,11 +6,12 @@ packages = {
     "url_info": {"depth": "爬取层次,根据不同的层次使用不同的xpath", "url": "网址"},
     "content": "爬虫返回结果,最终结果或url集合,url集合:根据此次的depth寻找下一个depth,然后与url一起插入spider_id_list",
     "slave_id": "奴隶id",
-    "msg_code": "返回编码,1-失败,0-成功,若失败将url放入失败池中,成功则保存返回信息"
+    "ret_msg": "返回信息,成功,失败信息,若失败将url放入失败池中,成功则保存返回信息"
 }
 
 # redis数据结构
 spider_id_list = [{"depth": "爬取层次,根据不同的层次使用不同的xpath", "url": "网址"}, {"depth": "", "url": ""}]  # url集合
+error_list = [] # 失败池
 slave_id_set = ["slave_id_1", "slave_id_2", "..."]  # slave集合
 logout_set = ["slave_id_1", "slave_id_2", "..."]  # slave集合
 depth_list = ["xpath1", "xpath2", "xpath3", "..."]  # 严格保持顺序
