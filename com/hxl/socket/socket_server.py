bef89049b8e87cc79c018866ee5ec7de0dc14557
23099223 <23099223@qq.com>
2018-09-22 22:59:04 +0800

    完善socket交互,测试通过

--- com/hxl/socket/socket_server.py
+++ com/hxl/socket/socket_server.py
@@ -38,7 +38,10 @@ class Master(socketserver.BaseRequestHandler):
                 break
             elif self.mrmg.r_sismember("slave_id_set", slave_id) == 0 and sig_type != "login":
                 break
-            self.request.sendall(json.dumps(self.deal_send_data(data, sig_type)).encode('utf8'))
+            data = self.deal_send_data(data, sig_type)
+            if data is None:
+                break
+            self.request.sendall(json.dumps(data).encode('utf8'))
 
     def finish(self):
         """在handle()方法之后调用,完成一些清理的工作,如果setup()抛异常侧不会调用"""
@@ -55,21 +58,28 @@ class Master(socketserver.BaseRequestHandler):
 
     def deal_send_data(self, data: {}, sig_type: str):
         """处理发送信息"""
+        if self.mrmg.r_sismember("logout_set", data.get("slave_id")) != 0:
+            data["sig_type"] = "logout"
+            return data
         if sig_type == "login":
             slave_id = data.get("slave_id")
             self.mrmg.r_sadd("slave_id_set", slave_id)
             data["msg"] = "success"
             pass
         elif sig_type == "spide":
-            if data.get("sig_type") == "spide":
+            if data.get("sig_type") == "spide" and data.get("url_info") is not None:
                 self.save_ret(data)
-            # 将已爬取过的url从running池中删除
-            self.mrmg.r_hdel("running_map", data.get("url_info").get("url"))
+                # 将已爬取过的url从running池中删除
+                self.mrmg.r_hdel("running_map", data.get("url_info").get("url"))
             ###### 应该是原子的
             url_info = self.mrmg.r_lpop(data.get("spider_id") + "_list")
-            if url_info is None:
+            if url_info is None and self.mrmg.r_exists("running_map") != 1:
+                data["sig_type"] = "exit"
+            elif url_info is None:
                 data["sig_type"] = "wait"
+                data["url_info"] = url_info
             else:
+                url_info = eval(url_info)
                 data["url_info"] = url_info
                 # 将获取的url保存到running池中
                 self.mrmg.r_hset("running_map", url_info.get("url"), url_info.get("depth"))
@@ -79,27 +89,32 @@ class Master(socketserver.BaseRequestHandler):
             error_dcit["msg"] = data.get("ret_msg")
             self.mrmg.r_lpush("error_list", error_dcit)
             data = self.deal_send_data(data, "spide")
+        else:
+            return None
         return data
 
     def save_ret(self, data: {}):
         """根据depth获取下一个depth或者确定是否是最后一个depth保存结果"""
         url_info = data.get("url_info")
+        if url_info is None:
+            return
         depth = url_info.get("depth")
-        depth_list = self.mrmg.r_lrange("depth_list", 0, -1)
+        depth_list = self.mrmg.r_lrange(data.get("spider_id") + "_depth_list", 0, -1)
         length, dep_index = len(depth_list), depth_list.index(depth)
         if length == dep_index + 1:
             # 保存结果
-            self.mrmg.r_lpush("ret_list", data.get("content"))
+            self.mrmg.r_rpush("ret_list", *data.get("content"))
         else:
             # 保存url
             next_depth = depth_list[dep_index + 1]
             url_list = data.get("content")
             spider_list = [{"depth": next_depth, "url": url} for url in url_list]
-            self.mrmg.r_lpush(data.get("spider_id") + "_list", *spider_list)
+            print("spider_list:>", spider_list)
+            self.mrmg.r_rpush(data.get("spider_id") + "_list", *spider_list)
 
 
 if __name__ == "__main__":
     # 注册信号处理程序
     logging.info("PID = %s" % os.getpid())
-    server = socketserver.ThreadingTCPServer(('127.0.0.1', 3028,), Master)
+    server = socketserver.ThreadingTCPServer(('127.0.0.1', 9320,), Master)
     server.serve_forever()
--- com/hxl/test/requestsTest.py
+++ com/hxl/test/requestsTest.py
@@ -3,20 +3,37 @@
 
 
 import json
+import logging
 import os
-# 客户端
 import socket
 import time
 
+logging.basicConfig(format='%(asctime)s:%(levelname)s: %(message)s', level=logging.DEBUG)
 client = socket.socket()  # 定义协议类型,相当于生命socket类型,同时生成socket连接对象
-client.connect(('127.0.0.1', 3028))
-n = 10
+client.connect(('127.0.0.1', 9320))
+n = 100
+msg = {"sig_type": "login", "spider_id": "test_no_a", "slave_id": "slave_1", "url_info": None, "pid": str(os.getpid())}
 while n:
-    msg = {"sig_type": "login", "slave_id": "slave_3", "url": "www.baidu.com", "pid": str(os.getpid())}
     client.send(json.dumps(msg).encode('utf8'))
     data = client.recv(10240)  # 这里是字节1k
     data = json.loads(data, encoding="utf8")
     print("recv:>", data)
+    if data.get("sig_type") == "login" and data.get("msg") == "success":
+        data["sig_type"] = "spide"
+    elif data.get("sig_type") == "spide":
+        data["content"] = ["url2", "url2", "url3"]
+    elif data.get("sig_type") == "wait":
+        data["sig_type"] = "spide"
+        logging.info("收到wait信号,等待10秒")
+        time.sleep(10)
+    elif data.get("sig_type") == "exit":
+        logging.info("收到exit信号,退出")
+        break
+    elif data.get("sig_type") == "logout":
+        logging.info("收到logout信号,退出")
+        break
+    msg = data
+    print("msg:>", msg)
     n = n - 1
     time.sleep(1)
 time.sleep(1)
